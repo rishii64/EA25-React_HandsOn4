15a5115e06cabbdaeeb58a19f396bbd42ccc96cb
Saptarsi <saptarsismmitra@gmail.com>
2023-10-11 18:30:40 +0530

    first commit

---
+++ src/Hook.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Home from "./Home"
+import Student from "./Student"
+import Contact from "./Contact"
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
+
+export default function Hook() {
+  return (
+    <div >
+        <BrowserRouter>
+          <div id='links'>
+              <Link id='linkHead' to='/'>Home</Link>
+              <Link id='linkHead' to='/student'>Student</Link>
+              <Link id='linkHead' to='/contact'>Contact</Link>
+          </div>
+            {/* <h1>Route Component Page</h1> */}
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/student' element={<Student />} />
+                <Route path='/contact' element={<Contact />} />
+            </Routes>
+        </BrowserRouter>
+    </div>
+  )
+}
+
---
+++ src/Student.js
@@ -0,0 +1,73 @@
+import React from 'react'
+
+export default function Student() {
+  return (
+    <div className='stu'>
+      <div className='title'>
+        <h1>Student Details</h1>
+        <button id='btn'>Add New Student</button>
+      </div>
+      <table id='table' border={1}>
+        <thead>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Course</th>
+          <th>Batch</th>
+          <th>Change</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John</td>
+            <td>26</td>
+            <td>MERN</td>
+            <td>October</td>
+            <td><a href=''>Edit</a></td>
+          </tr>
+          <tr>
+            <td>Doe</td>
+            <td>25</td>
+            <td>MERN</td>
+            <td>November</td>
+            <td><a href=''>Edit</a></td>
+          </tr>
+          <tr>
+            <td>Biden</td>
+            <td>26</td>
+            <td>MERN</td>
+            <td>September</td>
+            <td><a href=''>Edit</a></td>
+          </tr>
+          <tr>
+            <td>Barar</td>
+            <td>22</td>
+            <td>MERN</td>
+            <td>September</td>
+            <td><a href=''>Edit</a></td>
+          </tr>
+          <tr>
+            <td>Christ</td>
+            <td>23</td>
+            <td>MERN</td>
+            <td>October</td>
+            <td><a href=''>Edit</a></td>
+          </tr>
+          <tr>
+            <td>Elent</td>
+            <td>24</td>
+            <td>MERN</td>
+            <td>November</td>
+            <td><a href=''>Edit</a></td>
+          </tr>
+          <tr>
+            <td>Harshita Sharma</td>
+            <td>24</td>
+            <td>MERN</td>
+            <td>October</td>
+            <td><a href=''>Edit</a></td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
