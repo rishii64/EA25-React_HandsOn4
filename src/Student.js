20db82db6c83f9f63515200bfa196a420501f7f4
Saptarsi <saptarsismmitra@gmail.com>
2023-10-11 18:40:17 +0530

    first commit

--- src/Student.js
+++ src/Student.js
@@ -21,49 +21,49 @@ export default function Student() {
             <td>26</td>
             <td>MERN</td>
             <td>October</td>
-            <td><a href=''>Edit</a></td>
+            <td><a href='/stuDetails'>Edit</a></td>
           </tr>
           <tr>
             <td>Doe</td>
             <td>25</td>
             <td>MERN</td>
             <td>November</td>
-            <td><a href=''>Edit</a></td>
+            <td><a href='/stuDetails'>Edit</a></td>
           </tr>
           <tr>
             <td>Biden</td>
             <td>26</td>
             <td>MERN</td>
             <td>September</td>
-            <td><a href=''>Edit</a></td>
+            <td><a href='/stuDetails'>Edit</a></td>
           </tr>
           <tr>
             <td>Barar</td>
             <td>22</td>
             <td>MERN</td>
             <td>September</td>
-            <td><a href=''>Edit</a></td>
+            <td><a href='/stuDetails'>Edit</a></td>
           </tr>
           <tr>
             <td>Christ</td>
             <td>23</td>
             <td>MERN</td>
             <td>October</td>
-            <td><a href=''>Edit</a></td>
+            <td><a href='/stuDetails'>Edit</a></td>
           </tr>
           <tr>
             <td>Elent</td>
             <td>24</td>
             <td>MERN</td>
             <td>November</td>
-            <td><a href=''>Edit</a></td>
+            <td><a href='/stuDetails'>Edit</a></td>
           </tr>
           <tr>
             <td>Harshita Sharma</td>
             <td>24</td>
             <td>MERN</td>
             <td>October</td>
-            <td><a href=''>Edit</a></td>
+            <td><a href='/stuDetails'>Edit</a></td>
           </tr>
         </tbody>
       </table>
